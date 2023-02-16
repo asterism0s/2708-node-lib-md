4286a647b66b36facb23b7f7ccab39163a067a16
asterism0s <sarahtradsocial@gmail.com>
2023-02-16 16:38:37 +0100

    Changing import method

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-console.log('olá mundo');
+const chalk = require('chalk');
+
+//import chalk from 'chalk';
+
+console.log(chalk.blue('Hello world!'));
+
 console.log('São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer)');
 console.log('São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer)');
