73f6b9af573a8ed37dec171592d925978efb0e33
asterism0s <sarahtradsocial@gmail.com>
2023-02-21 16:53:47 +0100

    Creating error function

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 import fs from 'fs';
 import chalk from 'chalk';
 
+function controlError (error) {
+     throw new Error (error);
+}
 
 function getFile (filePath) {
      const encoding = 'UTF-8';
-     fs.readFile(filePath, encoding, (_, text) => {
+     fs.readFile(filePath, encoding, (error, text) => {
+          if (error) {
+               controlError (error);
+          }
           console.log (chalk.green(text));
      })
 }
