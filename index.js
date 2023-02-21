7b612a20ea71cf21b3e5584ac6cc29fc41ef36fd
asterism0s <sarahtradsocial@gmail.com>
2023-02-21 16:40:33 +0100

    Creating function to get the files

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-
-
+import fs from 'fs';
 import chalk from 'chalk';
 
-console.log(chalk.blue('Hello world!'));
 
-console.log('São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer)');
-console.log('São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer)');
+function getFile (filePath) {
+     const encoding = 'UTF-8';
+     fs.readFile(filePath, encoding, (_, text) => {
+          console.log (chalk.green(text));
+     })
+}
+
+getFile ('./arquivos/texto.md');
