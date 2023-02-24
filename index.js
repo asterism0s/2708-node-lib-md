9cffb77655046df162a3ce2e7375a1f34e7c272e
asterism0s <sarahtradsocial@gmail.com>
2023-02-24 10:16:00 +0100

    Refactoring extractLinks function

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 import fs from 'fs';
 import chalk from 'chalk';
 
-const textTest = 'São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.).';
-
 function controlError (error) {
      console.log(error);
      throw new Error (chalk.red(error.code, 'Missing file or directory'));
@@ -10,20 +8,19 @@ function controlError (error) {
 
 function extractLinks (text) {
      const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
-     const captures = regex.exec(text);
-     console.log(captures);
+     const captures = [...text.matchAll(regex)];
+     const results = captures.map (captures => ({[captures[1]]: captures[2]}));
+     return results;
 }
-
-extractLinks (textTest);
 
 async function getFile (filePath) {
      try {
           const encoding = 'UTF-8';
           const text = await fs.promises.readFile(filePath, encoding)
-          console.log(chalk.green(text));
+          console.log (extractLinks (text));
      } catch (error) {
           controlError(error);
      }
 }
 
-// getFile ('./arquivos/texto.md');
+getFile ('./arquivos/texto.md');
